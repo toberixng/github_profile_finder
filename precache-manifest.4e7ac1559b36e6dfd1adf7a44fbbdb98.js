38590114666c46e93840bfe1e967b1a79463343a
toberixng <tobyfatumo@gmail.com>
2019-10-15 19:53:15 +0100

    Updates

---
+++ precache-manifest.4e7ac1559b36e6dfd1adf7a44fbbdb98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7467656686fd43c08bb5307c997d076",
+    "url": "/github_profile_finder/index.html"
+  },
+  {
+    "revision": "76398e7f9892a39da046",
+    "url": "/github_profile_finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "34fb360db6c897cccbb5",
+    "url": "/github_profile_finder/static/js/2.c3bacb96.chunk.js"
+  },
+  {
+    "revision": "76398e7f9892a39da046",
+    "url": "/github_profile_finder/static/js/main.81f99562.chunk.js"
+  },
+  {
+    "revision": "f67011e40c8bf1aa68f3",
+    "url": "/github_profile_finder/static/js/runtime-main.f87bd114.js"
+  }
+]);
